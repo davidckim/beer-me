3782e983aa398d9a2d9556fc6f651bf31803ca74
Tina White <dr.tina.white@gmail.com>
2015-01-23 11:25:47 -0800

    Make a seed file

--- beer-me/server/db/seeds.rb
+++ beer-me/server/db/seeds.rb
@@ -6,8 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-location1 = Location.create({name: "a bar", address: "somewhere in sf", zip_code: 94110})
-location2 = Location.create({name: "another bar", address: "somewhere not in sf", zip_code: 23948})
+location1 = Location.create({name: "21st Amendment BrewPub", address: "123 A street, San Francisco, CA", zip_code: 94110})
+location2 = Location.create({name: "Ziggy's Ale House", address: "3049 Ale Lane, Stout-Lager, ABV", zip_code: 23948})
 
 beer1 = Beer.create(
 	{name: "Monk's Blood", 
@@ -22,13 +22,13 @@ beer1.locations << location2
 
 
 
-location3 = Location.create({name: "a bar", address: "somewhere in sf", zip_code: 94110})
-location4 = Location.create({name: "another bar", address: "somewhere not in sf", zip_code: 23948})
+location3 = Location.create({name: "The Beer Hall", address: "9023 Haight St, San Francisco, CA", zip_code: 94110})
+location4 = Location.create({name: "Chug'n'Go", address: "209 Porter Rd., Barrel Aged, OG", zip_code: 76555})
 
 beer2 = Beer.create(
-	{name: "Monk's Blood", 
-		abv: "9.5%", 
-		rating: "*****", 
+	{name: "Shallow Grave Porter", 
+		abv: "6.5%", 
+		rating: "****", 
 		price: "$8.75", 
 	}
 ) 				
@@ -37,14 +37,14 @@ beer2.locations << location3
 beer2.locations << location4
 
 
-location5 = Location.create({name: "a bar", address: "somewhere in sf", zip_code: 94110})
-location6 = Location.create({name: "another bar", address: "somewhere not in sf", zip_code: 23948})
+location5 = Location.create({name: "The Alchemist", address: "74 Hop Avenue, San Francisco, CA", zip_code: 94110})
+location6 = Location.create({name: "Hofbrau", address: "293 Barley Way, Pale Ale, IPA", zip_code: 23948})
 
 beer3 = Beer.create(
-	{name: "Monk's Blood", 
-		abv: "9.5%", 
+	{name: "Full Boar Scotch Ale", 
+		abv: "6.2%", 
 		rating: "*****", 
-		price: "$8.75", 
+		price: "$6.50", 
 	}
 ) 				
 
