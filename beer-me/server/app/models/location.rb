b43ade059a61ce5a7da7270f0357d7dba840ab4b
Tina White <dr.tina.white@gmail.com>
2015-01-24 09:20:05 -0800

    Moved server to separate repo

--- beer-me/server/app/models/location.rb
+++
@@ -1,6 +0,0 @@
-class Location < ActiveRecord::Base
-	has_and_belongs_to_many :beers
-	# write a method using regex to find the zip code from string input of full address
-	# be sure that no errors are thrown if the method does not find a zip code
-	
-end
--- beer-me/server/db/migrate/20150123190904_create_beers_locations.rb
+++
@@ -1,8 +0,0 @@
-class CreateBeersLocations < ActiveRecord::Migration
-  def change
-    create_table :beers_locations do |t|
-    	t.belongs_to :beer 
-    	t.belongs_to :location
-    end
-  end
-end
